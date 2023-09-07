07b4399e525b1c534d8084af334777c8306aaaa9
Fayçal Chebihi <chebihifaycal.it@gmail.com>
2023-09-07 15:48:02 +0100

    notes data

--- app/src/main/java/org/m2i/jetnote/data/NoteData.kt
+++ app/src/main/java/org/m2i/jetnote/data/NoteData.kt
@@ -5,7 +5,16 @@ import org.m2i.jetnote.model.Note
 class NoteDataSource {
     fun leadNotes() : List<Note> {
         return listOf(
-
+            Note(title = "A good day", description = "We went on a vacation by the lake"),
+            Note(title = "Android Compose", description = "Working on Android Compose course today"),
+            Note(title = "Keep at it...", description = "Sometimes things just happen"),
+            Note(title = "A movie day", description = "Watching a movie with family today"),
+            Note(title = "A movie day", description = "Watching a movie with family today"),
+            Note(title = "A movie day", description = "Watching a movie with family today"),
+            Note(title = "A movie day", description = "Watching a movie with family today"),
+            Note(title = "A movie day", description = "Watching a movie with family today"),
+            Note(title = "A movie day", description = "Watching a movie with family today"),
+            Note(title = "A movie day", description = "Watching a movie with family")
         )
     }
 }
