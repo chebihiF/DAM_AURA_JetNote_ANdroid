846062f71b842d969d131ecfb3d322157a28891c
Fayçal Chebihi <chebihifaycal.it@gmail.com>
2023-09-07 15:03:38 +0100

    add NoteButton

--- app/src/main/java/org/m2i/jetnote/components/NoteComponents.kt
+++ app/src/main/java/org/m2i/jetnote/components/NoteComponents.kt
@@ -1,7 +1,9 @@
 package org.m2i.jetnote.components
 
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 
 
@@ -28,7 +30,6 @@ fun NoteInputText(
     onImeAction: () -> Unit = {}
 ) {
     val keyboardController = LocalSoftwareKeyboardController.current
-
     TextField(
         value = text,
         onValueChange = { onTextChange },
@@ -45,4 +46,20 @@ fun NoteInputText(
         }),
         modifier = modifier)
 
+}
+
+@Composable
+fun NoteButton(
+    modifier: Modifier = Modifier,
+    text: String,
+    onClick: () -> Unit,
+    enabled: Boolean = true
+){
+    Button(
+        onClick = { onClick },
+        shape = CircleShape,
+        enabled = enabled,
+        modifier = modifier) {
+        Text(text = text)
+    }
 }
--- app/src/main/java/org/m2i/jetnote/screen/NoteScreen.kt
+++ app/src/main/java/org/m2i/jetnote/screen/NoteScreen.kt
@@ -1,6 +1,7 @@
 package org.m2i.jetnote.screen
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -9,16 +10,32 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import org.m2i.jetnote.R
+import org.m2i.jetnote.components.NoteButton
 import org.m2i.jetnote.components.NoteInputText
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun NoteScreen(){
+
+    var title by remember {
+        mutableStateOf("")
+    }
+
+    var description by remember {
+        mutableStateOf("")
+    }
+
     Scaffold(topBar = {
         CenterAlignedTopAppBar(
             title = { Text(text = stringResource(id = R.string.app_name)) },
@@ -29,9 +46,26 @@ fun NoteScreen(){
     })
 
     { paddingValues ->
-        Column(modifier = Modifier.padding(paddingValues)) {
-            NoteInputText(text = "hello", label = "hello",
+        Column(
+            modifier = Modifier
+                .padding(paddingValues)
+                .fillMaxSize(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+
+            NoteInputText(
+                modifier = Modifier.padding(top = 9.dp, bottom = 8.dp),
+                text = title,
+                label = "Title",
                 onTextChange = {})
+
+            NoteInputText(
+                modifier = Modifier.padding(top = 9.dp, bottom = 8.dp),
+                text = description,
+                label = "Add Note",
+                onTextChange = {})
+
+            NoteButton(text = "Save", onClick = { /*TODO*/ })
         }
     }
 }
