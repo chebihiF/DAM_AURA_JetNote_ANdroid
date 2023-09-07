fe557c8b9341af3642bc26713f90ccd6dfd9fe27
Fayçal Chebihi <chebihifaycal.it@gmail.com>
2023-09-07 14:24:06 +0100

    add appBar theme

--- app/src/main/java/org/m2i/jetnote/screen/NoteScreen.kt
+++ app/src/main/java/org/m2i/jetnote/screen/NoteScreen.kt
@@ -4,10 +4,13 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import org.m2i.jetnote.R
@@ -17,7 +20,11 @@ import org.m2i.jetnote.R
 fun NoteScreen(){
     Scaffold(topBar = {
         CenterAlignedTopAppBar(
-            title = { Text(text = stringResource(id = R.string.app_name)) })
+            title = { Text(text = stringResource(id = R.string.app_name)) },
+            colors = TopAppBarDefaults.centerAlignedTopAppBarColors(
+                containerColor = MaterialTheme.colorScheme.primary,
+                titleContentColor = Color.White
+            ))
     })
 
     { paddingValues ->
